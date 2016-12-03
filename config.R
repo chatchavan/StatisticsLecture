705adf18cad27e0b39bbc6127a45012a751f32a1
Chat Wacharamanotham <chat@ifi.uzh.ch>
2016-12-03 13:43:50 +0100

    Restructure views and move text to a separate file (temporarily)

--- config.R
+++ config.R
@@ -1,7 +1,7 @@
 kDefaultDatatableOptions <- 
 
-displayTable <- function(df, height = 100) {
-  datatable(df, class="compact", height = height, 
+displayTable <- function(df, height = 200) {
+  datatable(df, class="compact", height = height+50, 
             options = list(
               searching = F,
               paging = F,
